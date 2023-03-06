ed24140a61ff7c645223743ab6c381bcbaf1209f
Erkus00 <aragon1399@protonmail.com>
2023-03-06 12:31:56 +0100

    Ejercicio de Examen: DONE

--- src/main/java/com/example/springbasics/repo/AlumnoRepo.java
+++ src/main/java/com/example/springbasics/repo/AlumnoRepo.java
@@ -7,9 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface AlumnoRepo extends JpaRepository<Alumno, Long> {
-    @Query(value = "SELECT al FROM Alumno al WHERE al.ad<5")
+    @Query(value = "SELECT al FROM Alumno al WHERE al.ad<5.0")
     List<Alumno> findByAdSuspensos();
 
-    @Query(value = "SELECT al FROM Alumno al WHERE al.id<5")
+    @Query(value = "SELECT al FROM Alumno al WHERE al.di<5.0")
     List<Alumno> findByIdSuspensos();
 }
